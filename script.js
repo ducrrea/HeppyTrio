b0b613fea3577df02edea442f7e72ffbd09755fc
kmaiap <kaillany.proenca@aluno.senai.br>
2026-03-26 16:01:48 -0300

    varias coresss

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Exemplo de uso:
+document.body.style.backgroundColor = getRandomColor();
+console.log(getRandomColor()); // Ex: #6ba0be
+
 const botao = document.getElementById('btnCor');
 
 botao.addEventListener('click', function() {
     // Altera a cor de fundo do body
-    document.body.style.backgroundColor = 'lightblue';
+    document.body.style.backgroundColor = getRandomColor();
 });
